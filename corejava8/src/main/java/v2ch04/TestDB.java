17fa824dbeab82d921402b2bc08423f597f54a04
liujinguang <jinguang.liu@qq.com>
2016-12-06 22:59:45 +0800

    update TestDB

--- corejava8/src/main/java/v2ch04/TestDB.java
+++ corejava8/src/main/java/v2ch04/TestDB.java
@@ -3,6 +3,7 @@ package v2ch04;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,28 +13,89 @@ import java.util.Properties;
 public class TestDB {
 
     public TestDB() {
-    }
-
-    public static void runTest() throws SQLException, IOException {
-        Connection conn = getConnection();
         try {
-            Statement stat = conn.createStatement();
-            stat.executeUpdate("CREATE TABLE IF NOT EXISTS Greetings(message char(20))");
-            stat.executeUpdate("INSERT INTO Greetings VALUES('Hello world')");
-
-            ResultSet result = stat.executeQuery("SELECT * FROM Greetings");
-            if (result.next()) {
-                System.out.println(result.getString(1));
-            }
-            result.close();
-
-            stat.execute("DROP TABLE Greetings");
-        } finally {
-            conn.close();
+            conn = getConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() throws IOException, SQLException {
+    public void runTest() throws SQLException, IOException {
+        Statement stat = conn.createStatement();
+        stat.executeUpdate("CREATE TABLE IF NOT EXISTS Greetings(message char(20))");
+        stat.executeUpdate("INSERT INTO Greetings VALUES('Hello world')");
+
+        ResultSet result = stat.executeQuery("SELECT * FROM Greetings");
+        if (result.next()) {
+            System.out.println(result.getString(1));
+        }
+        result.close();
+
+        stat.execute("DROP TABLE Greetings");
+
+        stat.close();
+    }
+
+    public void runAutoCommitTest() {
+        Statement stat;
+        try {
+            stat = conn.createStatement();
+            conn.setAutoCommit(false);
+            stat.executeUpdate("CREATE TABLE IF NOT EXISTS Greetings(message char(20))");
+//            stat.executeUpdate("CREATE TABLE Greetings(message char(20))");
+            stat.executeUpdate("INSERT INTO Greetings VALUES('Hello world')");
+            conn.commit();
+            conn.setAutoCommit(true);
+
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+        
+
+        
+
+//        ResultSet result = stat.executeQuery("SELECT * FROM Greetings");
+//        if (result.next()) {
+//            System.out.println(result.getString(1));
+//        }
+//        result.close();
+//
+//        stat.execute("DROP TABLE Greetings");
+//
+//        stat.close();
+    }
+
+    public void runBatchTest() throws SQLException, IOException {
+        System.out.println("=====runBatchTest=====");
+        Statement stat = conn.createStatement();
+
+        stat.addBatch("CREATE TABLE IF NOT EXISTS Greetings(message char(20))");
+        stat.addBatch("INSERT INTO Greetings VALUES('Hello world')");
+        stat.addBatch("INSERT INTO Greetings VALUES('Hello Java')");
+        stat.addBatch("INSERT INTO Greetings VALUES('Hello Core')");
+        stat.executeBatch();
+
+        ResultSet result = stat.executeQuery("SELECT * FROM Greetings");
+        while (result.next()) {
+            System.out.println(result.getString(1));
+        }
+        result.close();
+
+        stat.execute("DROP TABLE Greetings");
+
+        stat.close();
+        System.out.println("=====Done=====");
+    }
+
+    private Connection getConnection() throws IOException, SQLException {
         Properties pros = new Properties();
 
         FileInputStream in = new FileInputStream(TestDB.class.getResource(
@@ -53,14 +115,14 @@ public class TestDB {
         return DriverManager.getConnection(url, user, password);
     }
 
-    public static void main(String[] args) {
-        // Properties props = System.getProperties();
-        // for (Object obj : props.keySet()) {
-        // System.out.println(obj + "=" + props.getProperty((String)obj));
-        // }
+    private Connection conn;
 
+    public static void main(String[] args) {
         try {
-            runTest();
+            TestDB testDB = new TestDB();
+            testDB.runTest();
+            testDB.runBatchTest();
+            testDB.runAutoCommitTest();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (IOException e) {
