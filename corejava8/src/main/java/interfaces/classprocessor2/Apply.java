604f15ec8e8e9dda239ef54c9762d4aacc05d8fd
liujinguang <jinguang.liu@qq.com>
2017-04-27 22:27:19 +0800

    interfaces

--- corejava8/src/main/java/interfaces/classprocessor2/Apply.java
+++ corejava8/src/main/java/interfaces/classprocessor2/Apply.java
@@ -4,6 +4,7 @@ import static net.mindview.util.Print.print;
 
 public class Apply {
     public static void process(Processor p, Object s) {
-        print();
+        print("Using processor " + p.name());
+        print(p.process(s));
     }
 }
